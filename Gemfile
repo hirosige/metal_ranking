75cc3e9c9bd3667f0a44e024fa75b740305f5592
hirosige <hirosige1@gmail.com>
2014-11-03 00:07:26 +0700

    Make a basic User model

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.9'
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'sprockets', '2.11.0'
+gem 'bcrypt-ruby', '3.1.2'
 
 # Use mysql as the database for Active Record
 group :development, :test do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home Page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Metal Ranking')
     end
 
     it "should have the title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("Metal Ranking(tmp) | Home")
     end
   end
@@ -16,12 +16,12 @@ describe "StaticPages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Metal Ranking(tmp) | Help")
     end
   end
@@ -29,13 +29,26 @@ describe "StaticPages" do
   describe "About page" do
 
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("Metal Ranking(tmp) | About")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title" do
+      visit contact_path
+      expect(page).to have_title("Metal Ranking(tmp) | Contact")
     end
   end
 
