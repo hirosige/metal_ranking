e35a0690f5b67344b2b80eb50190f37e3e0899d5
hirosige <hirosige1@gmail.com>
2014-11-08 22:07:36 +0700

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save {self.email = email.downcase}
+  before_create :create_remember_token
 
   # attr_accessible :email, :name, :password, :password__confirmation
 
@@ -18,4 +19,18 @@ class User < ActiveRecord::Base
 
   validates :password_confirmation, presence: true,
             length: {minimum: 6}
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 end
